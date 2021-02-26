58c230ed24217466eaaaa7ec028fbb6dd82f3b5c
Şebnem Arlı <ssebnemarlii@gmail.com>
2021-02-26 23:14:19 +0300

    26

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    //sabit olduğu için static 
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün İsmi Geçersiz";
+        public static string MaintenanceTime="Sistem Bakımda";
+        public static string ProductsListed="Ürünler Listelendi";
+    }
+}
